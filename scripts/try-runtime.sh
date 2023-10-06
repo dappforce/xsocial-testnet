8a15572f7d5c05e772a402a2e47adffac6d9850c
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2023-10-06 22:24:46 +0300

    Add build runtime and update try runtime scripts

--- scripts/try-runtime.sh
+++ scripts/try-runtime.sh
@@ -8,4 +8,4 @@ set -e
 cargo build --release --locked --features try-runtime --workspace
 "$ROOT_DIR"/target/release/xsocial-node try-runtime \
 --runtime "$ROOT_DIR/target/release/wbuild/xsocial-runtime/xsocial_runtime.compact.compressed.wasm" \
---chain=dev on-runtime-upgrade live --uri wss://xsocial.subsocial.network:443
+--chain=dev on-runtime-upgrade --checks=all live --uri wss://xsocial.subsocial.network:443
